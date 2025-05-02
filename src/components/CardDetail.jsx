24781e3aeb7153c0b84d1a61c68f404851da2fb3
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-05-02 16:56:24 -0700

    Created a fetch call to gather specifics of a clicked pokemon and made it to where pokemon names are capitalized

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,10 +1,11 @@
 const CardDetail = ({ poke }) => {
    return (
       <div className="card-detail">
-         <h2>{poke.name}</h2>
+         <h2>{poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}</h2>
+         <img src={poke.sprites.front_default} alt={poke.name}/>
          <p>Height: {poke.height}</p>
          <p>Weight: {poke.weight}</p>
-         <p>Type: {poke.types}</p>
+         <p>Type: {poke.types.map((type) => type.type.name).join(', ')}</p>
       </div>
    );
 };
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,18 +4,29 @@ import CardDetail from './CardDetail'
 // Importing pokemon props from App.jsx
 const Cards = ({ pokemon }) => {
 
+   // Use to conditionally render a pokemon card
    const [poke, setPoke] = useState();
 
+   // Making a fetch call to fetch details of specific pokemon id
+   const onPokemonClick = async (id) => {
+      // Gathers all data related to that pokemon
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+      const data = await res.json();
+      setPoke(data);
+   }
 
    return(
       <div className="cards">
          {pokemon.results?.map((pokemon, index) => (
+            
+            
             <div
                key={index}
                className="pokeCard"
+               onClick={() => onPokemonClick(pokemon.url.split('/')[6])}
             >
 
-               {pokemon.name}
+               {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
             </div>
          ))}
 
